a3e189ffe1387edadff0f080f350adf90e9718f0
Charly Ay <Charly1986-ay@users.noreply.github.com>
2026-04-07 18:17:54 -0300

    instalacion de sqlmodel

---
+++ app/core/db.py
@@ -0,0 +1,16 @@
+from typing import Iterator
+
+from sqlmodel import Session, create_engine, SQLModel
+from app.core.config import settings
+
+engine = create_engine(
+    settings.DATABASE_URL, echo=True, 
+    connect_args={'check_same_thread': False} if 'sqlite' in settings.DATABASE_URL else {}
+)
+
+def init_db() -> None:
+    SQLModel.metadata.create_all(engine) # dev
+
+def get_session() -> Iterator[Session]:
+    with Session(engine) as session:
+        yield session
