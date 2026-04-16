62bd2aacda6e0ee23f87c39a24b34c1d2b56bb22
Charly Ay <Charly1986-ay@users.noreply.github.com>
2026-04-16 18:16:47 -0300

    creando dependencias

---
+++ app/api/routers/auth_router.py
@@ -0,0 +1,34 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlmodel import Session
+
+from app.api.deps import get_db, DBSession
+from app.models.user import UserCreate, UserRead
+from app.repositories.user_repository import UserRepository
+from app.services.auth_services import AuthServices
+
+router = APIRouter(prefix='/auth', tags=['Auth'])
+
+
+@router.post(
+    '/register', 
+    response_model=UserRead, 
+    status_code=status.HTTP_201_CREATED
+)
+def resgister(playload: UserCreate, db: DBSession):
+    service = AuthServices(UserRepository(db=db))
+    return service.register(playload=playload)
+
+
+@router.post('/login')
+def login(email: str, password: str, db: DBSession):
+    service = AuthServices(UserRepository(db=db))
+    token = service.login(email=email, password=password)
+    return {'access_token': token, 'token_type': 'bearer'}
+
+
+@router.post('/token')
+def login(db: DBSession, form: OAuth2PasswordRequestForm = Depends()):
+    service = AuthServices(UserRepository(db=db))
+    token = service.login(email=email, password=password)
+    return {'access_token': token, 'token_type': 'bearer'}
